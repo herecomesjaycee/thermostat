812be5b6d7d03392c6ca396e2a8f0edb1623249b
James Turner <mail.jt@gmail.com>
2017-01-31 14:36:53 +0000

    added basic powersaving

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -4,39 +4,57 @@ describe ("Thermostat", function() {
   var thermostat;
   var defaultTemp;
   var defaultMin;
+  var defaultMaxSave;
 
   beforeEach(function() {
     thermostat = new Thermostat;
     defaultTemp = thermostat._STARTING;
-    defaultMin = thermostat._MINTEMPERATURE
+    defaultMin = thermostat._MIN_TEMP;
+    defaultMaxSave = thermostat._MAX_TEMP_SAVE;
   });
+  describe("power saving on",function() {
+    describe("#initialize", function() {
+      it ("returns initial temperature as the default", function() {
+        expect(thermostat.temperature()).toEqual(defaultTemp);
+      });
+    });
 
-  describe("#initialize", function() {
-    it ("returns initial temperature as the default", function() {
-      expect(thermostat.temperature()).toEqual(defaultTemp);
+    describe("#tempUp", function() {
+      it ("increases temp by one", function() {
+        thermostat.tempUp();
+        expect(thermostat.temperature()).toEqual(defaultTemp + 1);
+      });
+    });
+
+    describe("#tempDown", function() {
+      it ("decrease temp by one", function() {
+        thermostat.tempDown();
+        expect(thermostat.temperature()).toEqual(defaultTemp - 1);
+      });
+    });
+
+    describe("Min temperature is 10 degrees", function() {
+      it ("throw error if temperature goes below min value", function() {
+        for(var i = defaultTemp; i > defaultMin; i--){thermostat.tempDown()}
+        var error = "temperature cannot go below minimum value"
+        expect(function() {thermostat.tempDown()}).toThrow(error);
+      });
     });
   });
-
-  describe("#tempUp", function() {
-    it ("increases temp by one", function() {
-      thermostat.tempUp();
-      expect(thermostat.temperature()).toEqual(defaultTemp + 1);
+  describe("power saving off",function() {
+    beforeEach(function() {
+      thermostat = new Thermostat;
+      thermostat.setPowerSave(false);
+    });
+    it ("can tell it's off", function() {
+      expect(thermostat.getPowerSave()).toEqual(false)
+    });
+    it ("can't go above 32 degrees", function(){
+      for(var i = defaultTemp; i < defaultMaxSave; i++){
+        thermostat.tempUp();
+      }
+      var error = "temperature cannot go above maximum value"
+      expect(function() {thermostat.tempUp()}).toThrow(error);
     });
   });
-
-  describe("#tempDown", function() {
-    it ("decrease temp by one", function() {
-      thermostat.tempDown();
-      expect(thermostat.temperature()).toEqual(defaultTemp - 1);
-    });
-  });
-
-  describe("Min temperature is 10 degrees", function() {
-    it ("throw error if temperature goes below min value", function() {
-      for(var i = 20; i > 10; i--){thermostat.tempDown()}
-      var error = "temperature cannot go below minimum value"
-      expect(function() {thermostat.tempDown()}).toThrow(error);
-    });
-  });
-
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,7 +1,10 @@
 function Thermostat() {
   this._STARTING = 20;
-  this._MINTEMPERATURE = 10;
+  this._MIN_TEMP = 10;
+  this._MAX_TEMP = 32;
+  this._MAX_TEMP_SAVE = 32;
   this._degrees = this._STARTING;
+  this._powerSave = true;
 };
 
 Thermostat.prototype.temperature = function() {
@@ -9,10 +12,18 @@ Thermostat.prototype.temperature = function() {
 };
 
 Thermostat.prototype.tempUp = function () {
+  if (this._degrees + 1 > this._MAX_TEMP){
+    throw "temperature cannot go above maximum value";
+  };
   this._degrees += 1;
 };
 
 Thermostat.prototype.tempDown = function () {
-  if (this._degrees - 1 < this._MINTEMPERATURE){throw "temperature cannot go below minimum value"}
+  if (this._degrees - 1 < this._MIN_TEMP){
+    throw "temperature cannot go below minimum value";
+  };
   this._degrees -= 1;
 };
+
+Thermostat.prototype.getPowerSave = function () {return this._powerSave};
+Thermostat.prototype.setPowerSave = function (args) {this._powerSave = args};

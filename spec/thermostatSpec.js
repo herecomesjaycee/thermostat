7b45a791422e820f2eef940579ef2b3519c3589a
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2017-01-31 12:56:50 +0000

    Thermostat cannot go below min temp

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -3,10 +3,12 @@
 describe ("Thermostat", function() {
   var thermostat;
   var defaultTemp;
+  var defaultMin;
 
   beforeEach(function() {
     thermostat = new Thermostat;
     defaultTemp = thermostat._STARTING;
+    defaultMin = thermostat._MINTEMPERATURE
   });
 
   describe("#initialize", function() {
@@ -21,4 +23,20 @@ describe ("Thermostat", function() {
       expect(thermostat.temperature()).toEqual(defaultTemp + 1);
     });
   });
+
+  describe("#tempDown", function() {
+    it ("decrease temp by one", function() {
+      thermostat.tempDown();
+      expect(thermostat.temperature()).toEqual(defaultTemp - 1);
+    });
+  });
+
+  describe("Min temperature is 10 degrees", function() {
+    it ("throw error if temperature goes below min value", function() {
+      for(var i = 20; i > 10; i--){thermostat.tempDown()}
+      var error = "temperature cannot go below minimum value"
+      expect(function() {thermostat.tempDown()}).toThrow(error);
+    });
+  });
+
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,5 +1,6 @@
 function Thermostat() {
   this._STARTING = 20;
+  this._MINTEMPERATURE = 10;
   this._degrees = this._STARTING;
 };
 
@@ -9,4 +10,9 @@ Thermostat.prototype.temperature = function() {
 
 Thermostat.prototype.tempUp = function () {
   this._degrees += 1;
+};
+
+Thermostat.prototype.tempDown = function () {
+  if (this._degrees - 1 < this._MINTEMPERATURE){throw "temperature cannot go below minimum value"}
+  this._degrees -= 1;
 };

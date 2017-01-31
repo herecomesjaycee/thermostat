f2c38dd3e11328476a2f955d04678985e201dcfb
James Turner <mail.jt@gmail.com>
2017-01-31 16:40:27 +0000

    finished hooking up buttons to model

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 thermostat = new Thermostat();
 
+
 $(document).ready(function(){
-    $("#tempUp").click(function(){
-        thermostat.tempUp();
-        $("#temperatureDisplay").text("temp: "+thermostat._degrees);
-    });
-    $("#tempDown").click(function(){
-        thermostat.tempDown();
-        $("#temperatureDisplay").text("temp: "+thermostat._degrees);
-    });
-    $("#reset").click(function(){
-        thermostat.reset();
-        $("#temperatureDisplay").text("temp: "+thermostat._degrees);
-    });
-    $("#powerSave").click(function(){
-        thermostat.togglePowerSave();
-        $("#powerSaveDisplay").text("power save: "+thermostat.getPowerSave());
-    });
+  $("#tempUp").click(function(){
+    thermostat.tempUp();
+    updateDisplay();
+  });
+  $("#tempDown").click(function(){
+    thermostat.tempDown();
+    updateDisplay();
+  });
+  $("#reset").click(function(){
+    thermostat.reset();
+    updateDisplay();
+  });
+  $("#powerSave").click(function(){
+    thermostat.togglePowerSave();
+    updateDisplay();
+  });
 });
+
+var updateDisplay = function(){
+  $("#temperatureDisplay").text("temp: "+thermostat._degrees);
+  $("#powerSaveDisplay").text("power save: "+thermostat.getPowerSave());
+  $("#energyUsageDisplay").text("energy usage: "+thermostat.energy());
+}

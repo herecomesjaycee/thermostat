fcadcd6c781db5b9786bf7fae26312305a870a6d
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2017-01-31 15:59:36 +0000

    Added JQuery

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-thermostat = new Thermostat
+thermostat = new Thermostat();
+
+$(document).ready(function(){
+    $("#tempUp").click(function(){
+        thermostat.tempUp();
+        $("#temperatureDisplay").text("temp: "+thermostat._degrees);
+    });
+    $("#tempDown").click(function(){
+        thermostat.tempDown();
+        $("#temperatureDisplay").text("temp: "+thermostat._degrees);
+    });
+    $("#reset").click(function(){
+        thermostat.reset();
+        $("#temperatureDisplay").text("temp: "+thermostat._degrees);
+    });
+    $("#powerSave").click(function(){
+        thermostat.togglePowerSave();
+        $("#powerSaveDisplay").text("power save: "+thermostat.getPowerSave());
+    });
+});

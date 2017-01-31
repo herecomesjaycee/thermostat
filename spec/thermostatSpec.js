238312b4e3146471c2129839901774155c5aae82
James Turner <mail.jt@gmail.com>
2017-01-31 12:25:36 +0000

    second businesslogic + refactoring

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,13 +1,24 @@
+"use strict";
+
 describe ("Thermostat", function() {
   var thermostat;
+  var defaultTemp;
 
   beforeEach(function() {
-    thermostat = new Thermostat
+    thermostat = new Thermostat;
+    defaultTemp = thermostat._STARTING;
   });
 
   describe("#initialize", function() {
-    it ("returns initial temperature as 20", function() {
-      expect(thermostat.temperature()).toEqual(20)
+    it ("returns initial temperature as the default", function() {
+      expect(thermostat.temperature()).toEqual(defaultTemp);
+    });
+  });
+
+  describe("#tempUp", function() {
+    it ("increases temp by one", function() {
+      thermostat.tempUp();
+      expect(thermostat.temperature()).toEqual(defaultTemp + 1);
     });
   });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,7 +1,12 @@
 function Thermostat() {
-  this._degrees = 20;
+  this._STARTING = 20;
+  this._degrees = this._STARTING;
 };
 
 Thermostat.prototype.temperature = function() {
   return this._degrees;
+};
+
+Thermostat.prototype.tempUp = function () {
+  this._degrees += 1;
 };

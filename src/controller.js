38889f027898c1b20dd75ac527065e6f164203fc
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2017-02-01 15:10:38 +0000

    Changed CSS

--- src/controller.js
+++ src/controller.js
@@ -28,11 +28,5 @@ var updateDisplay = function(){
   $("#temperature").text(thermostat.temperature());
   $("#powerSave").text(thermostat.powerSaveDisplay());
   $("#powerUsage").text(thermostat.usage());
-  if(thermostat.usage()== 'low-usage'){
-    $('#powerUsage').css('color', 'green')
-  } else if (thermostat.usage()== 'medium-usage') {
-    $('#powerUsage').css('color', 'black')
-  } else {
-    $('#powerUsage').css('color', 'red')
-  }
+  $('#powerUsage').attr('class', thermostat.usage());
 };
